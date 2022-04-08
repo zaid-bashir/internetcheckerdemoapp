e4b3ce3cbee844b1f1ade5382469c18f0d476552
zaid-bashir <zaidwani2282@gmail.com>
2022-04-08 14:41:36 +0530

    First Commit Setup App

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Offline Internet Check',
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Flutter Offline Internet Check"),
+      ),
+      body: SafeArea(
+        child: Center(
+          child: Text("Internet Checker Testing..."),
+        ),
+      ),
+    );
+  }
+}
