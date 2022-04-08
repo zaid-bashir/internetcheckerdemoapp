3d88d3d7af6ffc8df694cc8d16742da56e56c161
zaid-bashir <zaidwani2282@gmail.com>
2022-04-08 16:06:34 +0530

    Updated Code Updates

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, unnecessary_string_interpolations
 
 import 'package:flutter/material.dart';
-
+import 'package:flutter_offline/flutter_offline.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Offline Internet Check',
       theme: ThemeData(
-        primarySwatch: Colors.orange,
+        primarySwatch: Colors.blue,
       ),
       home: const HomePage(),
     );
@@ -32,9 +32,52 @@ class HomePage extends StatelessWidget {
         title: Text("Flutter Offline Internet Check"),
       ),
       body: SafeArea(
-        child: Center(
-          child: Text("Internet Checker Testing..."),
+        child: OfflineBuilder(
+        connectivityBuilder: (
+          BuildContext context,
+          ConnectivityResult connectivity,
+          Widget child,
+        ) {
+          final bool connected = connectivity != ConnectivityResult.none;
+          return  Stack(
+            fit: StackFit.expand,
+            children: [
+              Positioned(
+                height: 24.0,
+                left: 0.0,
+                right: 0.0,
+                child: Container(
+                  color: connected ? Color(0xFF00EE44) : Color(0xFFEE4400),
+                  child: Center(
+                    child: Text("${connected ? 'ONLINE' : 'OFFLINE'}"),
+                  ),
+                ),
+              ),
+              connected ? Center(
+                child:  Text(
+                  'Internet Connected!',
+                ),
+              ) :  Center(
+                child:  Text(
+                  'Internet Disconnected!',
+                ),
+              ),
+            ],
+          );
+        },
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          // ignore: prefer_const_literals_to_create_immutables
+          children: <Widget>[
+             Text(
+              'There are no bottons to push :)',
+            ),
+             Text(
+              'Just turn off your internet.',
+            ),
+          ],
         ),
+      ),
       ),
     );
   }
